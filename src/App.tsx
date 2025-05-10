8f03fb2879038006d78f43d8185fdbbc24d14067
Doruk Özer <dorukozer@protonmail.com>
2025-05-11 02:39:33 +0300

    feat: tailwind initialized & some css updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,5 @@
-export const App = () => <h1>Hello World</h1>;
+export const App = () => (
+  <div className='w-full h-full bg-black flex justify-center items-center'>
+    <h1 className='text-5xl text-white'>Hello again world</h1>
+  </div>
+);
