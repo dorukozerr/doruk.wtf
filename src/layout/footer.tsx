f6e8d847f7eb8b4de0270aef09d7f8767cff216f
Doruk Özer <dorukozer@protonmail.com>
2025-05-11 15:19:33 +0300

    feat: footer conditional rendering emoji

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,5 +1,11 @@
-export const Footer = () => (
-  <header className='fixed bottom-4 left-1/2 z-20 flex -translate-x-1/2 rounded-md bg-gradient-to-r from-white/10 to-black/50 px-8 py-4 backdrop-blur-2xl'>
-    <span className='text-white'>Made with ❤️ </span>
-  </header>
-);
+import { useSceneStateContext } from '~/context/scene-state';
+
+export const Footer = () => {
+  const { isHovered } = useSceneStateContext();
+
+  return (
+    <header className='fixed bottom-4 left-1/2 z-20 flex -translate-x-1/2 rounded-md bg-gradient-to-r from-white/10 to-black/50 px-8 py-4 backdrop-blur-2xl'>
+      <span className='text-white'>Made with {isHovered ? '🔥' : '❤️'} </span>
+    </header>
+  );
+};
