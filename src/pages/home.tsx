50e9127f8b85e29828c153a38b792b5e8459f5e3
Doruk Özer <dorukozer@protonmail.com>
2025-05-15 05:51:33 +0300

    feat: scroll snapping added to sections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,9 +9,9 @@ export const Home = () => (
     initial='enter'
     animate='active'
     exit='exit'
-    className='flex h-full w-full flex-col items-start justify-start overflow-x-hidden overflow-y-auto'
+    className='flex h-full w-full snap-y snap-mandatory flex-col items-start justify-start overflow-x-hidden overflow-y-auto'
   >
-    <section className='flex min-h-dvh w-full items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -25,7 +25,7 @@ export const Home = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -9,9 +9,9 @@ export const Projects = () => (
     initial='enter'
     animate='active'
     exit='exit'
-    className='flex h-full w-full flex-col items-start justify-start overflow-x-hidden overflow-y-auto'
+    className='flex h-full w-full snap-y snap-mandatory flex-col items-start justify-start overflow-x-hidden overflow-y-auto'
   >
-    <section className='flex min-h-dvh w-full items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -25,7 +25,7 @@ export const Projects = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -47,7 +47,7 @@ export const Projects = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -73,7 +73,7 @@ export const Projects = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -97,7 +97,7 @@ export const Projects = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
@@ -122,7 +122,7 @@ export const Projects = () => (
         <div className='w-full flex-1' />
       </div>
     </section>
-    <section className='flex min-h-dvh w-full flex-col items-center justify-center'>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
