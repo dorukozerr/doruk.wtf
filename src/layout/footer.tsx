6949257bb528d5de3f1e6ed4311bfe93732d0f92
Doruk Özer <dorukozer@protonmail.com>
2025-05-27 04:46:31 +0300

    fix: footer html tag change

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -5,11 +5,11 @@ export const Footer = () => {
   const { isHovered } = useSceneStateContext();
 
   return (
-    <header className='fixed bottom-4 left-1/2 z-20 flex w-max -translate-x-1/2 items-center justify-center gap-2 rounded-md bg-gradient-to-r from-white/10 to-black/50 px-8 py-4 backdrop-blur-2xl'>
+    <footer className='fixed bottom-4 left-1/2 z-20 flex w-max -translate-x-1/2 items-center justify-center gap-2 rounded-md bg-gradient-to-r from-white/10 to-black/50 px-8 py-4 backdrop-blur-2xl'>
       <span className='text-white'>
         Made with {isHovered ? '🔥' : '❤️'} and
       </span>
       <VimIcon width={22} height={22} color='#fff' />
-    </header>
+    </footer>
   );
 };
