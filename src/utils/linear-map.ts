92ae69168e9411777421167426e43fdfa62270e3
Doruk Özer <dorukozer@protonmail.com>
2025-05-27 06:36:22 +0300

    refactor: util function jsdoc info

--- src/utils/linear-map.ts
+++ src/utils/linear-map.ts
@@ -1,15 +1,24 @@
-// transform a number from its own range to desired range
-//
-// example
-// 2 in range of 0, 4 = 15 in range of 10, 20
-//
-// parameters
-// from, from-, from+, to-, to+
-
+/**
+ * Maps a number from one range to another using linear interpolation
+ *
+ * @param value The input value to be mapped
+ * @param inMin Lower bound of the input range
+ * @param inMax Upper bound of the input range
+ * @param outMin Lower bound of the output range
+ * @param outMax Upper bound of the output range
+ * @returns The mapped value in the output range
+ *
+ * @example
+ * // Maps 2 from range [0,4] to range [10,20]
+ * console.log(linearMap(2, 0, 4, 10, 20)) // logs 15
+ */
 export const linearMap = (
-  f: number,
-  fm: number,
-  fp: number,
-  tm: number,
-  tp: number
-) => (f * (tm - tp)) / (fp - fm) + (-fm * (tm - tp)) / (fp - fm) + tp;
+  value: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number
+) =>
+  (value * (outMin - outMax)) / (inMax - inMin) +
+  (-inMin * (outMin - outMax)) / (inMin - inMax) +
+  outMax;
