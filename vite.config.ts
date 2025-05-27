0292f5cab6303a5655fb1c7f80252ad6fdb3e1a8
Doruk Özer <dorukozer@protonmail.com>
2025-05-27 07:30:14 +0300

    refactor: project deps updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import path from 'path';
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+// import ReactCompilerConfig from 'babel-plugin-react-compiler';
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import unusedCode from 'vite-plugin-unused-code';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic'
+      // babel: { plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]] }
+    }),
+    tailwindcss(),
+    unusedCode({ patterns: ['src/**/*.{ts,tsx}'] }),
+    visualizer({ open: true, filename: 'bundle-visualization.html' })
+  ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src')
