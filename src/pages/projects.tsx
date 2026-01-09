bf5c2f4745cc4471ed9a805903ab53c9b5fdf968
Doruk Özer <dorukozer@protonmail.com>
2026-01-09 04:55:57 +0300

    feat: add new project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -100,6 +100,27 @@ export const Projects = () => (
       <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
         <div className='w-full flex-1' />
         <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
+          <h3 className='text-center text-xl text-white'>fzf-clipboard</h3>
+          <div className='h-0.5 w-full bg-linear-to-r from-white/0 via-white/30 to-white/0' />
+          <p className='mx-auto max-w-sm text-center text-xs font-light text-white/80'>
+            fzf wrapper + background process script for clipboard history search
+            like any other fzf extension -{' '}
+            <a
+              href='https://github.com/dorukozerr/fzf-clipboard'
+              target='_blank'
+              className='text-white underline'
+            >
+              link
+            </a>
+          </p>
+        </div>
+        <div className='w-full flex-1' />
+      </div>
+    </section>
+    <section className='flex min-h-dvh w-full snap-center items-center justify-center'>
+      <div className='flex h-[min(90vw,610px)] w-[min(90vw,610px)] flex-col items-center justify-center gap-4 overflow-hidden rounded-full bg-black/50'>
+        <div className='w-full flex-1' />
+        <div className='w-full space-y-4 bg-black/50 p-4 sm:p-8'>
           <h3 className='text-center text-xl text-white'>Video Diary App</h3>
           <div className='h-0.5 w-full bg-linear-to-r from-white/0 via-white/30 to-white/0' />
           <p className='text-center text-xs font-light text-white/80'>
